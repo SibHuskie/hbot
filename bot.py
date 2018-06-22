95a90164f32d68b0f1290837c73971cecda81c47
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-06-22 19:04:40 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,4 +16,10 @@ async def on_ready():
     print(client.user.id)
     print("---------------")
     await client.change_presence(game=discord.Game(name='on Horny Hubbies (18+)'))
+    
+@client.event
+async def on_message(message, timeout=10,):
+    await client.process_commands(message)
+    if message.content.lower().startswith('hey new bot'):
+        await client.send_message(message.channel, "HELLLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
 client.run(os.environ['BOT_TOKEN'])
