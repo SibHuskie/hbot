df72998f7c666bc46f539e21377abbe03e7b37c6
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-06-23 10:50:11 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import random
 import time
 import os
 
-client = commands.Bot(command_prefix="hb")
+client = commands.Bot(command_prefix="h!")
 footer_text = "Horny Hubbies"
 
 @client.event
@@ -17,15 +17,12 @@ async def on_ready():
     print("---------------")
     await client.change_presence(game=discord.Game(name='on Horny Hubbies (18+)'))
     
-@client.event
-async def on_message(message, timeout=10,):
-    await client.process_commands(message)
-    if message.content.lower().startswith('hey new bot'):
-        await client.send_message(message.channel, "HELLLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-        
-    if message.content.lower().startswith('hbmc'):
-        msg = discord.Embed(colour=0xc90000, description= "")
-        msg.title = ":closed_book: Member Count :closed_book:"
-        msg.add_field(name="MEMBERS", value=(len(ctx.message.server.members)), inline=True)
-        await client.say(embed=msg)
+# membercount
+@client.command(pass_context=True)
+async def mc(ctx):
+    author = ctx.message.author
+    msg = discord.Embed(colour=0xcc1625, description= "")
+    msg.title = ":closed_book: Member Count :closed_book:"
+    msg.add_field(name="MEMBERS", value=(len(ctx.message.server.members)), inline=True)
+    await client.say(embed=msg)
 client.run(os.environ['BOT_TOKEN'])
