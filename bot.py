e46b8068eb975c1866c2f4f6aedd525782c9a4fb
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-06-23 10:56:49 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,4 +46,41 @@ async def ping(ctx):
     print("}ping")
     print("{} ### {}".format(author, author.id))
     print("============================================================")
+    
+# }ship <user1> <user2>
+@client.command(pass_context=True)
+async def ship(ctx, userName1: discord.Member = None, userName2: discord.Member = None):
+    percent = random.randint(0, 101)
+    author = ctx.message.author
+    msg = discord.Embed(colour=0xcc1625, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if userName1 == None or userName2 == None:
+        msg.add_field(name=":bangbang: ",value="`h!ship <user1> <user2>`")
+    else:
+        if percent >= 1 and percent <= 10:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Shit\n```\n:sob: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 11 and percent <= 20:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Awful\n```\n:cry: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 21 and percent <= 30:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Really Bad\n```\n:frowning2: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 31 and percent <= 40:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Bad\n```\n:slight_frown: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 41 and percent <= 50:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Okay\n```\n:neutral_face: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 51 and percent <= 60:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Good\n```\n:slight_smile: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 61 and percent <= 70:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Very Good\n```\n:smiley: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 71 and percent <= 80:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Fantastic\n```\n:blush: ".format(userName1.display_name, userName2.display_name, percent))
+        elif percent >= 81 and percent <= 90:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Amazing\n```\n:heart_eyes: ".format(userName1.display_name, userName2.display_name, percent))
+        else:
+            msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Perfect\n```\n:revolving_hearts: ".format(userName1.display_name, userName2.display_name, percent))
+    await client.say(embed=msg)
+    print("============================================================")
+    print("}ship <user1> <user2>")
+    print("{} ### {}".format(author, author.id))
+    print("============================================================")
 client.run(os.environ['BOT_TOKEN'])
